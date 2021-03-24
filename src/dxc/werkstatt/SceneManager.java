7614e0ebcabc9acdb83f8e1b060d5485ec14dd3f
Doner3000 <przemek476@gmail.com>
2021-03-24 11:21:57 +0100

    javafx

--- src/dxc/werkstatt/SceneManager.java
+++ src/dxc/werkstatt/SceneManager.java
@@ -1,10 +1,17 @@
 package dxc.werkstatt;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.HashMap;
+import java.util.Map;
+
 public class SceneManager {
-    public static final String START_SCENE;
-    public static final String REGELN_SCENE;
-    public static final String START_SCENE;
-    public static final String GAMEOVER_SCENE;
+    private static final String FXML_PATH = "fxml/";
+    public static final String START_SCENE = "startScene.fxml";
+    public static final String REGELN_SCENE = "regelnScene.fxml";
+    public static final String SPIEL_SCENE = "spielScene.fxml";
+    public static final String GAMEOVER_SCENE = "gameoverScene.fxml";
     private final Stage erstesStage;
 
+    private final Map<String, Scene> sceneMap = new HashMap<>();
 }
