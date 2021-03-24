78dda1d37dfb891229502131bce13cd954d4e82e
Doner3000 <przemek476@gmail.com>
2021-03-24 16:19:34 +0100

    neue Sachen

--- src/dxc/werkstatt/SceneManager.java
+++ src/dxc/werkstatt/SceneManager.java
@@ -13,7 +13,7 @@ public class SceneManager {
     private static final String FXML_PATH = "fxml/";
     public static final String START_SCENE = "startScene.fxml";
     public static final String REGELN_SCENE = "regelnScene.fxml";
-    public static final String SPIEL_SCENE = "spielScene.fxml";
+    public static final String SPIEL_SCENE = "spielScreen.fxml";
     public static final String GAMEOVER_SCENE = "gameoverScene.fxml";
     public static final String ABOUT_SCENE = "aboutScene.fxml";
     private Stage ersteStage;
