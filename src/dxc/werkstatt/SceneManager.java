ea36f59d719d9a85c7e0ec80504a6bda79d7a730
Doner3000 <przemek476@gmail.com>
2021-03-24 12:06:15 +0100

    SceneManager v1

--- src/dxc/werkstatt/SceneManager.java
+++ src/dxc/werkstatt/SceneManager.java
@@ -1,7 +1,11 @@
 package dxc.werkstatt;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,7 +15,49 @@ public class SceneManager {
     public static final String REGELN_SCENE = "regelnScene.fxml";
     public static final String SPIEL_SCENE = "spielScene.fxml";
     public static final String GAMEOVER_SCENE = "gameoverScene.fxml";
-    private final Stage erstesStage;
+    private Stage ersteStage;
 
     private final Map<String, Scene> sceneMap = new HashMap<>();
+    private final Map<String, Object> controllerMap = new HashMap<>();
+
+    private static SceneManager instance;
+
+    private SceneManager(){
+
+    }
+
+    public static SceneManager getInstance(){
+        if (instance == null){
+            instance = new SceneManager();
+            try {
+                instance.initScenes();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+                System.exit(1);
+            }
+        }
+        return instance;
+    }
+
+    public void initScenes() throws IOException{
+        loadScene(START_SCENE);
+        loadScene(REGELN_SCENE);
+        loadScene(SPIEL_SCENE);
+        loadScene(GAMEOVER_SCENE);
+    }
+
+    private void loadScene(String sceneKey) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getClassLoader().getResource(FXML_PATH + sceneKey));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Object controller = fxmlLoader.getController();
+
+        sceneMap.put(sceneKey, scene);
+        controllerMap.put(sceneKey, controller);
+    }
+
+    public void setPrimaryStage(Stage ersteStage){
+        this.ersteStage = ersteStage;
+    }
 }
