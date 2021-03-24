0bfdd932db421278e7ce2f8b9b22390b865965de
P4sc4l104 <psn_acc_sony@gmx.de>
2021-03-24 11:12:20 +0100

    resources

--- src/dxc/werkstatt/Figur.java
+++ src/dxc/werkstatt/Figur.java
@@ -3,4 +3,15 @@ package dxc.werkstatt;
 public class Figur {
     private String farbe;
     private int id;
+
+    /*
+        Orange: L
+        Gelb: O
+        Grün:  S
+        Rot: Z
+        Blau:J
+        Lila: T
+        Tuerkis:  I
+
+     */
 }
