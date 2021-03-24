38b3837bef80e6147dcca2d2205db2674d0f569e
P4sc4l104 <psn_acc_sony@gmx.de>
2021-03-24 11:18:40 +0100

    Farben mit Farbcodes

--- src/dxc/werkstatt/Figur.java
+++ src/dxc/werkstatt/Figur.java
@@ -5,13 +5,13 @@ public class Figur {
     private int id;
 
     /*
-        Orange: L
-        Gelb: O
-        Grün:  S
-        Rot: Z
-        Blau:J
-        Lila: T
-        Tuerkis:  I
+        Orange: L = rgb(255,165,0) #FFA500 hsl(39,100%,50%)
+        Gelb: O = rgb(255,255,0) #FFFF00 hsl(60,100%,50%)
+        Grün:  S = rgb(0,128,0) #008000 hsl(120,100%,25%)
+        Rot: Z = rgb(255,0,0) #FF0000 hsl(0,100%,50%)
+        Blau:J = rgb(0,0,255) #0000FF hsl(240,100%,50%)
+        Violett: T = rgb(238,130,238) #EE82EE hsl(300,76%,72%)
+        Zyan:  I = rgb(0,255,255) #00FFFF hsl(180,100%,50%)
 
      */
 }
