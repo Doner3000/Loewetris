03882ed0222b2f9e60ad356faae7aafe90dcb000
Doner3000 <przemek476@gmail.com>
2021-03-24 10:46:06 +0100

    Attribute für SceneManager

---
+++ src/dxc/werkstatt/SceneManager.java
@@ -0,0 +1,10 @@
+package dxc.werkstatt;
+
+public class SceneManager {
+    public static final String START_SCENE;
+    public static final String REGELN_SCENE = ;
+    public static final String START_SCENE;
+    public static final String GAMEOVER_SCENE;
+    private final Stage erstesStage;
+
+}
