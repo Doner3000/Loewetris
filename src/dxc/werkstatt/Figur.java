b481e1588aacbd358d6df65d5d2ce4c159d757d2
Doner3000 <przemek476@gmail.com>
2021-03-25 12:15:04 +0100

    spielScreen.fxml geändert

--- src/dxc/werkstatt/Figur.java
+++ src/dxc/werkstatt/Figur.java
@@ -8,7 +8,7 @@ public class Figur {
 
         Orange: L = rgb(255,165,0) #FFA500 hsl(39,100%,50%)
         Gelb: O = rgb(255,255,0) #FFFF00 hsl(60,100%,50%)
-        Grün:  S = rgb(0,128,0) #008000 hsl(120,100%,25%)
+        Schwarz:  S = rgb(255,255,255) #008000 hsl(120,100%,25%)
         Rot: Z = rgb(255,0,0) #FF0000 hsl(0,100%,50%)
         Blau:J = rgb(0,0,255) #0000FF hsl(240,100%,50%)
         Violett: T = rgb(238,130,238) #EE82EE hsl(300,76%,72%)
