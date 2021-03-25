04a979c1316005dac48ed9b8bb048020b523f579
Doner3000 <przemek476@gmail.com>
2021-03-25 09:30:03 +0100

    neue imports bei Controllern

--- src/dxc/werkstatt/RegelController.java
+++ src/dxc/werkstatt/RegelController.java
@@ -2,6 +2,8 @@ package dxc.werkstatt;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
