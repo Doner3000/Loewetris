d40874b64be1574cbba6828344f0a74377a51e76
LostLePaulus66 <paul_schenkel@web.de>
2021-03-25 09:23:37 +0100

    Buttonprogrammierung RegelnScene

--- src/dxc/werkstatt/RegelController.java
+++ src/dxc/werkstatt/RegelController.java
@@ -2,6 +2,8 @@ package dxc.werkstatt;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -30,15 +32,11 @@ public class RegelController {
     private TextArea regelscenetxt;
 
     @FXML
-    private Button regelsceneback;
+    private Button btnZurückRegelnScene;
 
     @FXML
-    void initialize() {
-        assert regelscenepane != null : "fx:id=\"regelscenepane\" was not injected: check your FXML file 'Untitled'.";
-        assert regelscenepng != null : "fx:id=\"regelscenepng\" was not injected: check your FXML file 'Untitled'.";
-        assert regelscenelabel != null : "fx:id=\"regelscenelabel\" was not injected: check your FXML file 'Untitled'.";
-        assert regelscenetxt != null : "fx:id=\"regelscenetxt\" was not injected: check your FXML file 'Untitled'.";
-        assert regelsceneback != null : "fx:id=\"regelsceneback\" was not injected: check your FXML file 'Untitled'.";
-
+    void btnBackPressed(ActionEvent event)
+    {
+        SceneManager.getInstance().showScene(SceneEnum.START_SCENE);
     }
 }
