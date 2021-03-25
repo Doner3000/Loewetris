2dc87618dab077cb50435e2b9caecc074a135e63
Doner3000 <przemek476@gmail.com>
2021-03-25 12:04:05 +0100

    wunderbar

--- src/dxc/werkstatt/SpielController.java
+++ src/dxc/werkstatt/SpielController.java
@@ -2,9 +2,12 @@ package dxc.werkstatt;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 public class SpielController {
@@ -22,8 +25,20 @@ public class SpielController {
     private Canvas canvasSpielScene;
 
     @FXML
-    void btnBackPressed(ActionEvent event) {
+    private GridPane gridSpielScene;
 
+    @FXML
+    void btnBackPressed(ActionEvent event) {
+        SceneManager.getInstance().showScene(SceneEnum.START_SCENE);
+    }
+
+    @FXML
+    private void initialize(){
+        GraphicsContext gc = canvasSpielScene.getGraphicsContext2D();
+        double width = canvasSpielScene.getWidth();
+        double height = canvasSpielScene.getHeight();
+
+        gc.strokeRect(0, 0, 30, 30);
     }
 
 }
